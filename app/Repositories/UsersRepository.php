fa5c6816a5a705bec10c9a8119352845eb92d58e
unknown <jobizzness@github.io>
2016-08-13 18:27:36 +0100

    fid

--- app/Repositories/UsersRepository.php
+++ app/Repositories/UsersRepository.php
@@ -9,11 +9,21 @@ class UsersRepository
 
     public function findByEmailOrCreate($userData)
     {
-        return User::firstOrCreate([
-            'email' => $userData->email
-        ])->with('profile', [
-            'username' => $userData->nickname,
-            'avatar' => $userData->avatar
-        ]);
+
+        //find if theres a user and return
+
+        $user = User::where('email', $userData->email)->first();
+
+        if(!$user){
+
+            $user = User::create(['email' => $userData->email]);
+            $profile = new Profile(
+                [
+                    'username' => $userData->nickname,
+                    'avatar' => $userData->avatar
+            ]);
+            $user->profile()->save($profile);
+        }
+
     }
 }
